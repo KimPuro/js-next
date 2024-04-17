0c57e2f8fba4e837cba1bc33e395f485f3a1be9d
KimPuro <puro@protonmail.ch>
2024-04-17 18:03:02 +0900

    fix : fix favicon href

--- app/layout.js
+++ app/layout.js
@@ -14,8 +14,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
     <head>
-      <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
-      <link rel="icon" href="favicon.ico" type="image/x-icon"/>
+      <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon"/>
+      <link rel="icon" href="/favicon.ico" type="image/x-icon"/>
     </head>
     <body className={inter.className}>{children}</body>
     </html>
